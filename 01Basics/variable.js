38b724110cd9d63c0e2da088e506e140672b81fb
Deepa Chaturvedi <Deepachaturvedi998@gmail.com>
2025-01-27 16:49:25 +0530

    first commit

---
+++ 01Basics/variable.js
@@ -0,0 +1,10 @@
+const accoutId = 2335875;
+let userName = "deeepa";
+var passward = 3448;
+city = "Bhopal";
+console.table([accoutId, userName, passward, city]);
+
+
+
+// prefer not to use var
+// because of issue in block scope and functional scope
