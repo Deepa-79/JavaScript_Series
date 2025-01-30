90e020dcbbb246788fa5a57301363b0dd1b00e58
Deepa Chaturvedi <Deepachaturvedi998@gmail.com>
2025-01-30 15:47:26 +0530

    basics strings

--- 01Basics/string.js
+++ 01Basics/string.js
@@ -1,3 +1,6 @@
 const name = "Deepa";
 repoCount = 30;
 console.log(`My name is ${name} and my repo is ${repoCount}`);
+console.log(name.lenght);
+console.log(name.length);
+console.log(name.toUpperCase());
