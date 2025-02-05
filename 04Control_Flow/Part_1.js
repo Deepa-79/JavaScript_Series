7ca4d49251fa020b00a4ba042bebcc26245a3966
deepa <Deepachaturvedi998@gmail.com>
2025-02-05 14:11:40 +0530

    ControlStatemt Part)1

---
+++ 04Control_Flow/Part_1.js
@@ -0,0 +1,59 @@
+//Operator : <,>,<=,>=, ==, !=, === (strict op check Data type also,) !==,
+
+// let marks = 300;
+// if (marks >= 200) {
+//   console.log("Average ");
+// }
+
+// let marks = 300;
+// if (marks >= 200) {
+//   console.log("Excellent ");
+// } else {
+//   console.log("Average");
+// }
+
+// truthy and faly Value
+
+// let EmailId = "Deepa Chaturvedi";
+// if (EmailId) {
+//   console.log("Got the Email");
+// } else {
+//   console.log("Not got");
+// }
+// output :Got the Email
+
+// let EmailId = "";
+// if (EmailId) {
+//   console.log("Got the Email");
+// } else {
+//   console.log("Not got");
+// }
+
+// output :Not got (if the variable not assign the value)
+
+// Falsy values : false, 0,-0, bigInt 0n, "", null,undifined, NaN
+
+// thruthy Values :  "0" , "false", " ", [], function(){}, {}
+
+//nullish coalsing operator (??) : null undefined
+
+// let val1;
+// val1 = 5 ?? 10;
+// console.log(val1); // 5
+
+// let val1;
+//  val1 = null ?? 10;
+// console.log(val1); // 10
+
+// let val1;
+// val1 = undefined ?? 15;
+// console.log(val1); // 15
+
+// let val1;
+// val1 = undefined ?? 15 ?? 10;
+// console.log(val1); // 15
+
+
+// Ternary Operator
+
+//condition ? true:False
