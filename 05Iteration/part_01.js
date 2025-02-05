1f23353a0c4aa7edc6e1f61743c1511cc227c744
deepa <Deepachaturvedi998@gmail.com>
2025-02-05 14:44:20 +0530

    iteration_Part_01

---
+++ 05Iteration/part_01.js
@@ -0,0 +1,60 @@
+// For loop
+
+// for (let index = 0; index <= 10; index++) {
+//   const element = index;
+//   console.log(element);  //1 to 10 numbers
+// }
+
+for (let i = 0; i <= 5; i++) {
+  console.log(`outer lop valuse is ${i}`);
+
+  for (let j = 0; j <= 5; j++) {
+    console.log(`inner loop value${j} And outer loop value is ${i}`);
+  }
+}
+
+
+// output
+
+// outer lop valuse is 0
+// inner loop value0 And outer loop value is 0
+// inner loop value1 And outer loop value is 0
+// inner loop value2 And outer loop value is 0
+// inner loop value3 And outer loop value is 0
+// inner loop value4 And outer loop value is 0
+// inner loop value5 And outer loop value is 0
+// outer lop valuse is 1
+// inner loop value0 And outer loop value is 1
+// inner loop value1 And outer loop value is 1
+// inner loop value2 And outer loop value is 1
+// inner loop value3 And outer loop value is 1
+// inner loop value4 And outer loop value is 1
+// inner loop value5 And outer loop value is 1
+// outer lop valuse is 2
+// inner loop value0 And outer loop value is 2
+// inner loop value1 And outer loop value is 2
+// inner loop value2 And outer loop value is 2
+// inner loop value3 And outer loop value is 2
+// inner loop value4 And outer loop value is 2
+// inner loop value5 And outer loop value is 2
+// outer lop valuse is 3
+// inner loop value0 And outer loop value is 3
+// inner loop value1 And outer loop value is 3
+// inner loop value2 And outer loop value is 3
+// inner loop value3 And outer loop value is 3
+// inner loop value4 And outer loop value is 3
+// inner loop value5 And outer loop value is 3
+// outer lop valuse is 4
+// inner loop value0 And outer loop value is 4
+// inner loop value1 And outer loop value is 4
+// inner loop value2 And outer loop value is 4
+// inner loop value3 And outer loop value is 4
+// inner loop value4 And outer loop value is 4
+// inner loop value5 And outer loop value is 4
+// outer lop valuse is 5
+// inner loop value0 And outer loop value is 5
+// inner loop value1 And outer loop value is 5
+// inner loop value2 And outer loop value is 5
+// inner loop value3 And outer loop value is 5
+// inner loop value4 And outer loop value is 5
+// inner loop value5 And outer loop value is 5
