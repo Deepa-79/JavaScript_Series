b38dd903fa626c8eca1ac449610755f867335a6e
deepa <Deepachaturvedi998@gmail.com>
2025-03-14 15:14:52 +0530

    commit

---
+++ object/app.js
@@ -0,0 +1,47 @@
+let student = {
+  name: "Deepa",
+  age: 22,
+  marks: 98,
+  city: "chhatarpur",
+};
+
+//Note :
+// Js automatically converts objcts keys to string.
+//even if we made the number as a key
+//the number will be converted to stings.
+
+let obj = {
+  1: "a",
+  2: "b",
+  true: "c",
+  null: "d",
+  undefined: "e",
+};
+
+//object of objects
+
+const classInfo = {
+  deepa: {
+    grade: "a+",
+    city: "chhatarpur",
+  },
+  pooja: {
+    grade: "a",
+    city: "Delhi",
+  },
+};
+
+//array of objects
+
+const studentInfo = [
+  {
+    name: "deepa",
+    grade: "a+",
+    city: "chhatarpur",
+  },
+  {
+    name: "pooja",
+    grade: "a",
+    city: "Delhi",
+  },
+];
